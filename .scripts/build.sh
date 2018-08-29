86fe77f71d0f9fc69400aa6f43f587e038ef9da2
James Fenn <18jafenn90@gmail.com>
2018-08-29 19:33:22 -0400

    updated script somewhat

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,4 +1,4 @@
-GOOS=linux GOARCH=386 go build -o AAH-linux-386
-GOOS=linux GOARCH=amd64 go build -o AAH-linux-amd64
-GOOS=linux GOARCH=arm go build -o AAH-linux-arm
-GOOS=linux GOARCH=arm64 go build -o AAH-linux-arm64
+GOOS=linux GOARCH=386 go build -ldflags "-linkmode external -extldflags -static" -o AAH-linux-386
+GOOS=linux GOARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -o AAH-linux-amd64
+GOOS=linux GOARCH=arm go build -ldflags "-linkmode external -extldflags -static" -o AAH-linux-arm
+GOOS=linux GOARCH=arm64 go build -ldflags "-linkmode external -extldflags -static" -o AAH-linux-arm64
