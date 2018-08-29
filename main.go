add9c9c5c07939715cbaafd960886efc0f67e999
James Fenn <18jafenn90@gmail.com>
2018-08-29 18:39:34 -0400

    fixed #4

--- main.go
+++ main.go
@@ -2,26 +2,47 @@ package main
 
 import (
 	"os"
+	"os/user"
+	"net/http"
 	"fmt"
 	"gopkg.in/yaml.v2"
+	"io"
 	"io/ioutil"
 )
 
 func main() {
-	pwd, _ := os.Getwd()
-	filePath := pwd + "/aahelp.yaml"
-	if _, err := os.Stat("~/.aahelp.yaml"); err == nil {
-		filePath = "~/.aahelp.yaml"
-	} else if _, err = os.Stat(filePath); err != nil {
-		fmt.Printf("default file '%v' doesn't exist\n", filePath)
+	user, _ := user.Current()
+	filePath := user.HomeDir + "/.config/aah/aahelp.yaml"
+	userFilePath := user.HomeDir + "/.aahelp.yaml"
+
+	if _, err := os.Stat(filePath); err != nil {
+		err := DownloadFile(filePath, "https://raw.githubusercontent.com/TheAndroidMaster/AAH/master/aahelp.yaml")
+		if (err == nil) {
+			main()
+		} else {
+			fmt.Printf("tried to download aahelp.yaml from TheAndroidMaster/AAH, didn't work\n%s\n", err)
+			fmt.Printf("please download the file to ~/.config/aah/aahelp.yaml yourself and the program will work\n")
+		}
+		
 		return
 	}
 
 	file, err := ioutil.ReadFile(filePath)
+	userFile, userErr := ioutil.ReadFile(userFilePath)
 	if err == nil {
 		m := make(map[interface{}]interface{})
 		err = yaml.Unmarshal([]byte(file), &m)
 		if err == nil {
+			if (userErr == nil) { 
+				m2 := make(map[interface{}]interface{})
+				err = yaml.Unmarshal([]byte(userFile), &m2)
+				if err == nil {
+					m = MergeMap(m, m2)
+				} else {
+					fmt.Printf("your file ~/.aahelp.yaml is not formatted correctly: %s\n", err)
+				}
+			}
+		
 			for i := 1; i < len(os.Args); i++ {
 				if val, ok := m[os.Args[i]]; ok {
 					if v, ok := val.(map[interface{}]interface{}); ok {
@@ -35,7 +56,7 @@ func main() {
 				}
 			}
 
-			print(nil, m)			
+			PrintMap(nil, m)			
 		} else {
 			fmt.Printf("err %v parsing file\n", err)
 		}
@@ -44,17 +65,54 @@ func main() {
 	}
 }
 
-func print(key, val interface{}) {
+func MergeMap(m1 map[interface{}]interface{}, m2 map[interface{}]interface{}) map[interface{}]interface{} {
+	for k, v := range m2 {
+		if val, ok := v.(map[interface{}]interface{}); ok && m1[k] != nil {
+			if val2, ok := m1[k].(map[interface{}]interface{}); ok {
+				m1[k] = MergeMap(val2, val)
+			} else {
+				m1[k] = v
+			}
+		} else {
+			m1[k] = v
+		}
+	}
+
+	return m1
+}
+
+func PrintMap(key, val interface{}) {
 	if v, ok := val.(map[interface{}]interface{}); ok {
 		if key != nil {
 			fmt.Printf("\n--> %s\n", key)
 		}
 		
 		for k, val := range v {
-			print(k.(string), val)
+			PrintMap(k.(string), val)
 		}
 		fmt.Printf("\n")
 	} else {
 		fmt.Printf("%-15s| \t\t%s\n", key, val)
 	}
+}
+
+func DownloadFile(path string, url string) error {
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
