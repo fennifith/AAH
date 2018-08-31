c99cd04d54b8d339132212c5ae7725a56e13883a
codekidX <ckrackerx15@gmail.com>
2018-08-31 12:11:26 +0530

    let go build know that your are using netgo since it exported externally

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,4 +1,5 @@
-GOOS=linux GOARCH=386 go build -ldflags "-linkmode external -extldflags -static" -o AAH-linux-386
-GOOS=linux GOARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -o AAH-linux-amd64
-GOOS=linux GOARCH=arm go build -ldflags "-linkmode external -extldflags -static" -o AAH-linux-arm
-GOOS=linux GOARCH=arm64 go build -ldflags "-linkmode external -extldflags -static" -o AAH-linux-arm64
+GOOS=linux GOARCH=386 go build -ldflags "-linkmode external -extldflags -static" -a -tags netgo -installsuffix netgo -o AAH-linux-386
+GOOS=linux GOARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -a -tags netgo -installsuffix netgo -o AAH-linux-amd64
+GOOS=linux GOARCH=arm go build -ldflags "-linkmode external -extldflags -static" -a -tags netgo -installsuffix netgo -o AAH-linux-arm
+GOOS=linux GOARCH=arm64 go build -ldflags "-linkmode external -extldflags -static" -a -tags netgo -installsuffix netgo -o AAH-linux-arm64
+
